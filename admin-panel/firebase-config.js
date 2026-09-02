7e285da8ea42a67c6671343cb7a22cd7f9112f03
riksan762-creator <riksan762@gmail.com>
2026-09-02 18:48:35 +0700

    Update Firebase configuration for admin panel

--- admin-panel/firebase-config.js
+++ admin-panel/firebase-config.js
@@ -1,13 +1,10 @@
-// Isi sama persis dengan firebase-config.js di folder user-app,
-// karena admin panel dan user app pakai project Firebase yang sama.
-
 const firebaseConfig = {
-  apiKey: "GANTI_DENGAN_API_KEY_FIREBASE",
-  authDomain: "GANTI.firebaseapp.com",
-  projectId: "GANTI_PROJECT_ID",
-  storageBucket: "GANTI.appspot.com",
-  messagingSenderId: "GANTI_SENDER_ID",
-  appId: "GANTI_APP_ID"
+  apiKey: "AIzaSyBV41WhaT_2MXYMAfEMJU0T2SE7_spODM8",
+  authDomain: "wa-crm-mini.firebaseapp.com",
+  projectId: "wa-crm-mini",
+  storageBucket: "wa-crm-mini.firebasestorage.app",
+  messagingSenderId: "57196443945",
+  appId: "1:57196443945:web:5c2c1db91af6cf3219df38"
 };
 
 firebase.initializeApp(firebaseConfig);
